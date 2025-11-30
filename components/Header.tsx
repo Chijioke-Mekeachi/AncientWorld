ab1a8185c26e5fac55bd639f6e26addf467bd2d7
chijioke-mekeachi <chijiokemekelachi@gmail.com>
2025-11-30 19:41:06 +0100

    updated title

--- components/Header.tsx
+++ components/Header.tsx
@@ -19,7 +19,7 @@ export default function Header() {
           {/* Logo */}
           <Link
             href="/"
-            className="text-3xl font-extrabold bg-gradient-to-r from-yellow-500 via-amber-600 to-red-500 bg-clip-text text-transparent flex items-center gap-3"
+            className="text-1xl font-extrabold bg-gradient-to-r from-yellow-500 via-amber-600 to-red-500 bg-clip-text text-transparent flex items-center gap-3"
           >
             <Image alt="Logo" src="/favorite.png" width={60} height={60} />
             Windows to The Ancient World
